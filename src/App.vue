8651f70c4c98fa36c669f0b36bde09c08d010f8b
BlackZhang01 <zhyong138@qq.com>
2021-11-24 23:18:59 +0800

    frist todolist

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-11-24 22:14:39
+ * @LastEditTime: 2021-11-24 23:15:14
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /Vue-learn-related/src/App.vue
+-->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Todo List</h1>
+    <input type="text" placeholder="Please input backlog, press Enter to confirm">
+    <ul>
+      <li>
+        <input type="checkbox">
+        sleep
+      </li>
+      <li>
+        <input type="checkbox">
+        eat rice
+      </li>
+      <li>
+        <input type="checkbox">
+        coding
+      </li>
+    </ul>
+    <div>
+      <input type="checkbox">
+      <span>selected 2</span>
+      /
+      <span>all 3</span>
+      <button>delete selected</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+export default {  
+  name:'App'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
